198b95b99eabf0b74fd6c404457e3274ecb1b473
pratheek7369 <pratheek13acharya@gmail.com>
2026-02-25 22:56:21 +0530

    Initial commit: Add Note-Taking App with .jsx structure

--- src/utils/auth.jsx
+++ src/utils/auth.jsx
@@ -1,23 +1,12 @@
-// src/utils/auth.jsx
-
 export const setToken = (token, username) => {
-  localStorage.setItem("jwt_token", token);
-  if (username) {
-    localStorage.setItem("user_id", username);
-  }
+  localStorage.setItem('jwt_token', token);
+  if (username) localStorage.setItem('user_id', username);
 };
 
-export const getToken = () => {
-  return localStorage.getItem("jwt_token");
-};
-
+export const getToken = () => localStorage.getItem('jwt_token');
 export const removeToken = () => {
-  localStorage.removeItem("jwt_token");
-  localStorage.removeItem("user_id");
+  localStorage.removeItem('jwt_token');
+  localStorage.removeItem('user_id');
 };
-
-export const getUserId = () => {
-  return localStorage.getItem("user_id");
-};
-
+export const getUserId = () => localStorage.getItem('user_id');
 export const isAuthenticated = () => !!getToken();
